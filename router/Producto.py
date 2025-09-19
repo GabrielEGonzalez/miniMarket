07a5c5265cc20ca4309dbe3a114b55a471bc8e80
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2025-09-18 21:41:49 -0500

    estructura de endpoint

--- router/Producto.py
+++ router/Producto.py
@@ -1,9 +1,30 @@
-from fastapi import FastAPI
+from fastapi import APIRouter
 from sqlmodel import SQLModel , Field , select
-
+from db import 
 class Producto(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)  # 🔑 PK obligatoria
+    id: int | None = Field(default=None, primary_key=True)
     nombre: str
     precio: float
     stock: int
     estado: int
+    
+class ProductoRead(SQLModel):
+    pass
+
+
+routerProducto = APIRouter(prefix="/producto",tags=["product"])
+
+@routerProducto.get("/")
+async def listaProducto():
+    pass
+    
+
+@routerProducto.post("/add")
+async def addProducto():
+    pass
+
+
+"""
+  * `/products` – listar productos
+  * `/products/add` – agregar producto
+"""
