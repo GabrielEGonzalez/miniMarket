933adc2da12da9e95343e759d10ecd75b9b3545e
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2025-09-20 15:39:03 -0500

    logica de extraccion de endpoint

---
+++ frontend/public/js/obtenerProducto.js
@@ -0,0 +1,42 @@
+let contendor = document.getElementById("contenedor");
+
+window.onload = function () {
+    fetch("http://127.0.0.1:8000/producto/").then(res => res.json()).then(data => {
+        
+        data.forEach(producto => {
+            let div = document.createElement("div");
+            let h2 = document.createElement("h2");
+            let p = document.createElement("p");
+            let div2 = document.createElement("div");
+            let img = document.createElement("img");
+            let div3 = document.createElement("div");
+            let button = document.createElement("button");
+            let h2precio = document.createElement("h2");
+            //img.src = 'https://cdn.iconscout.com/icon/premium/png-256-thumb/producto-terminado-icon-svg-download-png-8634015.png?f=webp';
+            img.src = `https://ui-avatars.com/api/?name=${producto.nombre}&size=200`;
+
+
+            div.classList.add("head-card");
+            div2.classList.add("body-card");
+            div3.classList.add("footer-card");
+
+            button.textContent = "Add to Cart"
+            h2.textContent = producto.nombre;
+            p.textContent= producto.descripcion;
+            h2precio.textContent = "$"+ producto.precio;
+
+            div3.appendChild(h2precio);
+            div3.appendChild(button);
+            div2.appendChild(img)
+            div2.appendChild(h2);
+            div2.appendChild(p)
+            div2.appendChild(div3);
+            div.appendChild(div2);
+            contendor.appendChild(div);
+        });
+        //console.log(data);
+    }).catch(error => {
+
+        console.error('Error al obtener los datos:', error);
+    });
+};
