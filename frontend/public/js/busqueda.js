1619174d8b794f3c06903d7ab2f38ebdd4cc1e0b
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2025-10-28 19:24:10 -0500

    nuevo script de busquda

---
+++ frontend/public/js/busqueda.js
@@ -0,0 +1,38 @@
+let productos = [];
+let contendor = document.getElementById("contenedor");
+let search = document.querySelector("#busqueda");
+
+fetch("http://127.0.0.1:8000/producto/").then(res => res.json()).then(data => {
+    console.log("Datos del backend:", data); 
+    productos = data;
+    mostrarDatos(productos);
+}).catch(e => console.error(e))
+
+
+search.addEventListener("input", function (e) {
+    let texto = e.target.value.toLowerCase();
+    let filtroProducto = productos.filter(prod =>
+        prod.nombre.toLowerCase().includes(texto)
+    )
+    mostrarDatos(filtroProducto);
+});
+
+function mostrarDatos(filtroProducto){
+    contendor.innerHTML = "";
+    filtroProducto.forEach(producto => {
+        let html = `
+      <div class="head-card">
+        <div class="body-card">
+          <img src="https://images.unsplash.com/photo-1626675431647-2dbf75028ec6?auto=format&fit=crop&q=80&w=400" alt="Imagen de producto" />
+          <h2>${producto.nombre}</h2>
+          <p>${producto.descripcion}</p>
+          <div class="footer-card">
+            <h2>$${producto.precio}</h2>
+            <button>Agregar al carrito</button>
+          </div>
+        </div>
+      </div>
+    `;
+        contendor.innerHTML += html;
+    });
+}
